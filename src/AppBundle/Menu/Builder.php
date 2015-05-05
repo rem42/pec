43ba305db0b78fb18e3c9d628f8e7f4b3fa1db3e
rem42 <remy492@hotmail.fr>
2015-05-05 17:23:00 +0200

    Mise en place du menu en version Bootstrap.

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -10,18 +10,37 @@ class Builder extends ContainerAware
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        if(!$this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))){
 
-        //$menu->addChild('Home', array('route' => 'homepage'));
+            $menu->addChild('Inscription', array('route' => 'register'))
+                ->setAttribute('icon', 'fa fa-list');
 
-        if($this->container->get('security.context')->isGranted('ROLE_USER')) {
-            $menu->addChild('Profil');
-            $menu['Profil']->addChild('Mon profil', array('route' => 'profil'));
-            $menu['Profil']->addChild('Deconnexion', array('route' => 'logout'));
+            $menu->addChild('Connexion', array('route' => 'login'))
+                ->setAttribute('icon', 'fa fa-group');
         }
-        else {
-            $menu->addChild('Inscription', array('route' => 'register'));
-            $menu->addChild('Connexion', array('route' => 'login'));
+
+
+        return $menu;
+    }
+
+    public function userMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+
+        if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {
+            $username = $this->container->get('security.context')->getToken()->getUser()->getUsername(); // Get username of the current logged in user
+            $menu->addChild('User', array('label' => 'Hi '.$username))
+                ->setAttribute('dropdown', true)
+                ->setAttribute('icon', 'fa fa-user');
+
+            $menu['User']->addChild('Mon profil', array('route' => 'profil'))
+                ->setAttribute('icon', 'fa fa-edit');
+            $menu['User']->addChild('Deconnexion', array('route' => 'logout'))
+                ->setAttribute('icon', 'fa fa-edit');
         }
+
 
         return $menu;
     }
