32fee1292eb66427a0415a44224e1cbede1716aa
rem42 <remy492@hotmail.fr>
2015-06-03 13:55:31 +0200

    edition et suppression des categories de skills

--- src/AppBundle/Controller/SkillCategoryController.php
+++ src/AppBundle/Controller/SkillCategoryController.php
@@ -9,19 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 class SkillCategoryController extends Controller{
 
-    public function addAction(Request $request){
+    public function listAction(Request $request){
 
-        $skillcategory = new SkillCategory();
+        $skillCategory = new SkillCategory();
 
-        $form = $this->createForm(new SkillCategoryAddType(), $skillcategory);
+        $form = $this->createForm(new SkillCategoryAddType(), $skillCategory);
         $form->handleRequest($request);
+        $error = false;
 
         if ($form->isValid()) {
-
-            $skillcategory = $form->getData();
-            $this->get('appbundle.repository.skillcategory')->save($skillcategory);
-
-            return $this->redirect($this->generateUrl('addskillcategory'));
+            $this->get('appbundle.repository.skillcategory')->save($skillCategory);
         }
 
         $skillcategories = $this->get('appbundle.repository.skillcategory')->liste();
@@ -29,7 +26,35 @@ class SkillCategoryController extends Controller{
         return $this->render('AppBundle:SkillCategory:add.html.twig', array(
             'skillcategories' => $skillcategories,
             'form' => $form->createView(),
+            'error' => $error,
         ));
+    }
+
+    public function deleteAction(Request $request){
+        $skillCategory = $this->get('appbundle.repository.skillcategory')->findById($request->get("id"), $this->getUser());
+        $this->get('appbundle.repository.skillcategory')->delete($skillCategory);
+
+        return $this->redirect($this->generateUrl('addskillcategory'));
+    }
+
+    public function editAction(Request $request){
+        if($request->get("id")!=""){
+            $skillCategory = $this->get('appbundle.repository.skillcategory')->findById($request->get("id"));
+            $form = $this->createForm(new SkillCategoryAddType(), $skillCategory);
+            $form->handleRequest($request);
+            $error = false;
+
+            if ($form->isValid()) {
+                $this->get('appbundle.repository.skillcategory')->save($skillCategory);
+                return $this->redirect($this->generateUrl('addskillcategory'));
+            }
+
+            return $this->render('@App/SkillCategory/edit.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }else{
+            return $this->redirect($this->generateUrl('addskillcategory'));
+        }
     }
 
     /*public function profileAction(){
